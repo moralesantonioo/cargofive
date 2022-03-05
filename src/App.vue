c201a3d980a56085cfd75f0db07a77da5f8e30d2
moralesantonio <jmoralesm99@hotmail.com>
2022-03-05 16:48:14 -0500

    primer avance

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,14 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <router-view/>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+@import url('https://fonts.googleapis.com/css2?family=Luxurious+Roman&family=Poppins:ital,wght@0,200;1,200&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Luxurious+Roman&family=Poppins:ital,wght@0,200;0,700;1,200&display=swap');
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+*{
+  margin: 0;
+  padding: 0;
+  font-family: 'Poppins', sans-serif !important;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,59 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <b-table striped hover :items="data" :current-page="currentPage" :per-page="perPage" ></b-table>
+    <b-pagination @change="nextData($event)" v-model="currentPage" :per-page="perPage" :total-rows="rows" pills size="sm" align="center"></b-pagination>
   </div>
+  
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      data: [],
+      perPage: null,
+      currentPage: null,
+      last_page: null,
+      total: null
+    }
+  },
+
+  mounted(){
+    this.cargofiveData(+1);
+  },
+
+  methods: {
+    async cargofiveData(page){
+      await axios.get(`http://apitest.cargofive.com/api/ports?page=${page}`).then(response => {
+        this.data = response.data.data
+        this.current_page = response.data.meta.current_page
+        //this.last_page = response.data.meta.last_page
+        this.perPage = response.data.meta.per_page
+        this.total = response.data.meta.total
+
+        console.log(response.data, 'data')
+        //console.log(this.last_page, 'last_page')
+        //console.log(this.perPage, 'perPage')
+        //console.log(this.total, 'total')
+        //console.log(this.current_page, 'current_page')
+      })
+      .catch(error => {
+        console.error(error)
+      })
+    },
+    async nextData(numberPage){
+      await this.cargofiveData(numberPage)
+      //console.log(pagina, 'entesa')
+    }
+  },
+
+  computed: {
+    rows() {
+      return this.total
+    }
   }
 }
 </script>
