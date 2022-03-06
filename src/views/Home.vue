ea2d9b19f6db437b6feb5ee8d77a3ef450171bc6
moralesantonio <jmoralesm99@hotmail.com>
2022-03-06 18:36:33 -0500

    examen cargofive terminado

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,59 +1,156 @@
 <template>
   <div class="container">
-    <b-table striped hover :items="data" :current-page="currentPage" :per-page="perPage" ></b-table>
-    <b-pagination @change="nextData($event)" v-model="currentPage" :per-page="perPage" :total-rows="rows" pills size="sm" align="center"></b-pagination>
+    <b-row class="filters"> <!-- inputs para realizar los filtros, se hace uso de un v-select y un v-input, css Bootstrap Vue  -->
+      <h1 class="title">Filtros: </h1>
+      <b-col>
+        <span>Seleccione opción a filtrar: </span>
+        <b-form-select v-model="selected" :options="options" class="v-select-style"></b-form-select>
+      </b-col>
+      <b-col>
+        <span class="search">Búsqueda: </span>
+        <b-form-input
+          @keyup="filters(busqueda)"
+          v-model="busqueda"
+          placeholder="Buscar"
+          type="text"
+          class="v-input-style"
+        ></b-form-input>
+      </b-col>
+    </b-row >
+    <!-- table en donde se muestran los datos al hacer el llamado a la api, css Bootstrap Vue  -->
+    <b-table 
+      class="table"
+      :items="data"
+      :responsive="true"
+      sticky-header="600px"
+    ></b-table>
+    <!-- paginación con sus datos respectivos que se traen de la api, css Bootstrap Vue  -->
+    <b-pagination
+      v-model="currentPage"
+      @change="nextData($event)"
+      :per-page="perPage"
+      :total-rows="rows"
+      pills
+      size="sm"
+      align="center"
+    ></b-pagination>
   </div>
-  
 </template>
 
 <script>
-import axios from 'axios'
+import axios from "axios";
 
 export default {
-  name: 'Home',
+  name: "Home",
   data() {
     return {
       data: [],
-      perPage: null,
-      currentPage: null,
-      last_page: null,
-      total: null
-    }
+      dataFilters: [],
+      perPage: '',
+      currentPage: '',
+      total: '',
+      busqueda: '',
+      selected: 'id',
+      options: ['id', 'country','continent']
+    };
   },
 
-  mounted(){
-    this.cargofiveData(+1);
+  mounted() {
+    this.cargofiveData();
   },
 
   methods: {
-    async cargofiveData(page){
-      await axios.get(`http://apitest.cargofive.com/api/ports?page=${page}`).then(response => {
-        this.data = response.data.data
-        this.current_page = response.data.meta.current_page
-        //this.last_page = response.data.meta.last_page
-        this.perPage = response.data.meta.per_page
-        this.total = response.data.meta.total
-
-        console.log(response.data, 'data')
-        //console.log(this.last_page, 'last_page')
-        //console.log(this.perPage, 'perPage')
-        //console.log(this.total, 'total')
-        //console.log(this.current_page, 'current_page')
-      })
-      .catch(error => {
-        console.error(error)
-      })
+    //Se realizó el uso de axios para poder hacer el llamado a la api y obtener los datos a utilizar
+    async cargofiveData(page) {
+      await axios
+        .get(`http://apitest.cargofive.com/api/ports?page=${page}`)
+        .then((response) => { 
+          this.data = response.data.data;
+          this.dataFilters = response.data.data;
+          this.current_page = response.data.meta.current_page;
+          this.perPage = response.data.meta.per_page;
+          this.total = response.data.meta.total;
+        })
+        .catch((error) => {
+          console.error(error);
+        });
     },
-    async nextData(numberPage){
-      await this.cargofiveData(numberPage)
-      //console.log(pagina, 'entesa')
-    }
+    // El metodo nextData es el metodo para la paginación, donde se llama al metodo cargofiveData y asi obtener los datos siguientes
+    async nextData(numberPage) {
+      await this.cargofiveData(numberPage);
+      //limpiar los inputs al momento de cambiar pagina 
+      this.busqueda = ''
+      this.selected = 'id'
+    },
+    // El metodo filters hace que al momento de hacer uso de los inputs pueda mostrar los datos que el usuario pida
+    filters(busqueda) {
+      if (busqueda.length === 0) {
+        this.data = this.dataFilters;
+      } else {
+        //se realiza el filtro a la data y se coloca un select repecto a lo que el usuario seleccione para filtrar
+        const dataFilter = this.data.filter(e => 
+          e[this.selected] == busqueda
+        );
+        //se realiza un .length para que me traigo los datos del array data y asi se pueda realizar los filtros
+        dataFilter.length > 0
+          ? (this.data = dataFilter)
+          : (this.data = this.dataFilters);
+      }
+    },
   },
 
   computed: {
     rows() {
-      return this.total
+      return this.total;
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+.container{
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  .filters{
+    width: 100%;
+    margin: 30px 0;
+    display: flex;
+    flex-direction: column;
+    .title{
+      font-size: 35px;
+      font-weight: bold;
+    }
+    .v-select-style{
+      width: 100%;
+      height: 38px;
+      border-radius: 3px;
+      border: 1px solid #D4D5D7;
+      color: #878889;
+      margin-bottom: 20px;
+    }
+    .search{
+      margin-top: 10px;
     }
   }
 }
-</script>
+@media (min-width: 768px){
+  .container{
+    .filters{
+      width: 100%;
+      margin: 30px 0;
+      display: flex;
+      flex-direction: row;
+      .v-select-style{
+        width: 100%;
+        height: 38px;
+        border-radius: 3px;
+        border: 1px solid #D4D5D7;
+        color: #878889;
+      }
+    }
+  }
+}
+</style>
